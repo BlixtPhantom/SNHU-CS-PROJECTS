02a55ca512549fb96ba8c393929cfa577139142f
Mason Williams <158350522+BlixtPhantom@users.noreply.github.com>
2024-09-18 20:22:58 -0400

    Add files via upload

---
+++ CS-210 Programming Languages/Clock.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+void Display12() {
+
+}
+void Display24() {
+
+}
+void DisplayMenu() {
+    cout << "****************************" << endl;
+    cout << "* Press 1 - Add One Hour   *" << endl;
+    cout << "* Press 2 - Add One Minute *" << endl;
+    cout << "* Press 3 - Add One Second *" << endl;
+    cout << "* Press 4 - Exit Program   *" << endl;
+    cout << "****************************" << endl;
+}
+int main() {
+    DisplayMenu();
+    return 0;
+}
